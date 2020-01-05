77df43a5e785c92d6805bbb9d747cc4f909dc5e1
aviral2004 <aviralg1975@gmail.com>
2020-01-05 18:23:55 +0530

    My Django Girls app, first commit

---
+++ website/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Volumes/MacHDD/aviralgupta/Documents/GCI/djangogirls/website/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
